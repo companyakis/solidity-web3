928cce8308e9cb318bb32bea3eb10636deac8ca6
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-12-19 19:25:02 +0300

    Create part-c-0008-address-storing-owner.sol

---
+++ part-c-0008-address-storing-owner.sol
@@ -0,0 +1,7 @@
+/*
+Your Goal: Store the Owner
+Create a public address state variable called owner on the contract
+Next create a constructor function which will store the msg.sender in owner
+
+*/
+
